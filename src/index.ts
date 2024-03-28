aafb24a43bd68476763302cd0a496c4858a7edf8
AyushRay06 <rayayush47@gmail.com>
2024-03-28 19:43:37 +0530

    Project-Setup

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@ app.get("/api/v1/blog/:id", (c) => {
   return c.text("get blog route")
 })
 
+app.get("/api/v1/blog/", (c) => {
+  return c.text("get blog route")
+})
+
 app.post("/api/v1/blog", (c) => {
   return c.text("signin route")
 })
@@ -26,3 +30,6 @@ app.put("/api/v1/blog", (c) => {
 })
 
 export default app
+//postgresql://rayayush47:7VKITYeyBRE8@ep-twilight-art-95085231.us-east-2.aws.neon.tech/DB1?sslmode=require
+
+//DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiYzg5M2U5MTEtZWIwNy00MmY2LThhOTYtYzYyOTg0MjdhMTk2IiwidGVuYW50X2lkIjoiYWM4ODkwZGUxNzBlYmY2MWM5Mjk3ZGMyMDkyODdkNDg1NzJkMGY5YWY4N2ExYWJiNzdjZmUzMGQ3MGZjMGZmZCIsImludGVybmFsX3NlY3JldCI6IjczYjU0YzkzLTE2MjQtNDNhYy1iNmUzLTRlMjcxMGM2MWQ0ZCJ9.30_q73_Mdq1sfy98rQYAFZm8swzsofkLbV9DJzG2f30"
