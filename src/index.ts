699d29677232680a9aea68131534d84c5b4edf9a
AyushRay06 <rayayush47@gmail.com>
2024-03-29 21:37:53 +0530

    Auth Added

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,16 @@
 import { Hono } from "hono"
-import { PrismaClient } from "@prisma/client/edge"
-import { withAccelerate } from "@prisma/extension-accelerate"
-
+import { userRouter } from "./routes/user"
+import { blogRouter } from "./routes/blog"
 // Create the main Hono app
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string
+    JWT_SECRET: string
   }
 }>()
 
-app.post("/api/v1/user/signup", async (c) => {
-  const body = await c.req.json()
-
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
-  try {
-    await prisma.user.create({
-      data: {
-        email: body.email,
-        name: body.name,
-        password: body.password,
-      },
-    })
-
-    return c.text("User Created")
-  } catch (e) {
-    c.status(411)
-    return c.text("User with the emial already exists")
-  }
-})
-
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route")
-})
-
-app.get("/api/v1/blog/:id", (c) => {
-  const id = c.req.param("id")
-  console.log(id)
-  return c.text("get blog route")
-})
-
-app.get("/api/v1/blog/", (c) => {
-  return c.text("get blog route")
-})
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("signin route")
-})
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("signin route")
-})
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
 
 export default app
 //postgresql://rayayush47:7VKITYeyBRE8@ep-twilight-art-95085231.us-east-2.aws.neon.tech/DB1?sslmode=require
